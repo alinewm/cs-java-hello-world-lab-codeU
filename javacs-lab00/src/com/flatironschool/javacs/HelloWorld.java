4154dfde2dbbd958082fbe61453f341b9d3926fa
alinewm <alinewada@gmail.com>
2016-04-18 13:59:23 -0400

    Implement get version

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -3,9 +3,12 @@ package com.flatironschool.javacs;
 public class HelloWorld {
     public static Double getVersion() {
         // I won't work!
+    	Double version = Double.parseDouble(System.getProperty("java.specification.version"));
+    	return version;
     }
 
     public static void main(String[] args) {
 	    // you can test the output of getVersion() here
+    	System.out.print(getVersion());
     }
 }
